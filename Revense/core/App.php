31a5854f6caec53e0ac4e7de1c27dff6912f931d
Okan Guner <okngnr@hotmail.com>
2016-02-19 00:28:55 +0200

    Route algoritması tamamlandı

--- Revense/core/App.php
+++ Revense/core/App.php
@@ -6,88 +6,118 @@ class Application {
         {
             global $routeCollection;
             
-            $pg = isset($_GET['pg']) ? $_GET['pg'] : null;
-            $pg = rtrim($pg, '/');
+            $page = isset($_GET['pg']) ? $_GET['pg'] : null;
+            $pg = rtrim($page, '/');
             $pg = explode('/', $pg);
+            $routeMatch = FALSE;
             
             foreach($routeCollection->routes as $route)
             {
-                $pattern = ltrim(rtrim($route->pattern, '/'), '/');
-                $pattern = explode('/', $pattern);
-                $routeDifference = array_diff($pg, $pattern);
-                $routeDifference2 = array_diff($pattern, $pg);
-                
-//                print_r($routeDifference);
-//                print '---------------------';
-//                print_r($routeDifference2);
-                
-                if(count($routeDifference) === 0 && count($routeDifference2) === 0 
-                   || (count($routeDifference2) > 0 && strpos(array_values($routeDifference2)[0], '}') !== false)) // match
+                if(ltrim(rtrim($page, '/'), '/') == ltrim(rtrim($route->pattern, '/'), '/') 
+                   || preg_match('/' . preg_replace('/\//i', '\\/', rtrim($page, '/')) . '\/{.+}/i', $route->pattern) == 1)
                 {
-                    print_r($route);
+                    $parameters = array();
                     
-                    if($_SERVER['REQUEST_METHOD'] != $route->method){
-                        header("HTTP/1.0 404 Not Found");
-                        die();
-                    }
-                    
-                    if(isset($route->action['Controller'])){
-                        $temp = explode('@', $route->action['Controller']);
-                        $file = 'app/controller/' . $temp[0] . '.php';
+                    if(preg_match('/{.+}/i', $route->pattern) == 1)
+                    {
+                        $mismatchKeyFound = FALSE;
+                        preg_match_all('/{.+?}/i', $route->pattern, $matches);
                         
-                        if (file_exists($file))
-                            require_once $file;
-                        
-                        $controller = new $temp[0];
-                        $parameters = array();
-                        
-                        foreach($routeDifference as $parameterKey)
+                        foreach($matches[0] as $match)
                         {
-                            $key = ltrim(rtrim($parameterKey, '}'), '{');
+                            $key = rtrim(ltrim($match, '{'), '}');
                             
                             if($route->method === 'POST')
                             {
-                                if(!isset($_POST[$key])){
-                                    header("HTTP/1.0 404 Not Found");
-                                    die();
-                                }
+                                if(!isset($_POST[$key])){ $mismatchKeyFound = TRUE; break; }
                                 
                                 $parameters[] = $_POST[$key];
                             }
                             else if($route->method === 'GET')
                             {
-                                if(!isset($_GET[$key])){
-                                    header("HTTP/1.0 404 Not Found");
-                                    die();
-                                }
+                                if(!isset($_GET[$key])){ $mismatchKeyFound = TRUE; break; }
                                 
                                 $parameters[] = $_GET[$key];
                             }
+                            else if($route->method === 'PUT')
+                            {
+                                parse_str(file_get_contents("php://input"),$putVars);
+                                
+                                if(!isset($putVars[$key])){ $mismatchKeyFound = TRUE; break; }
+                                
+                                $parameters[] = $putVars[$key];
+                            }
+                            else if($route->method === 'DELETE')
+                            {
+                                parse_str(file_get_contents("php://input"),$deleteVars);
+                                
+                                if(!isset($deleteVars[$key])){ $mismatchKeyFound = TRUE; break; }
+                                
+                                $parameters[] = $deleteVars[$key];
+                            }
                         }
                         
-                        if(isset($temp[1]))
-                            call_user_func_array(array($controller, $temp[1]), $parameters);
-                        else
-                            call_user_func_array(array($controller, 'index'), $parameters);
-                    }
-                    else if(isset($route->action['View'])){
-                        $layout = 'default';
+                        if($_SERVER['REQUEST_METHOD'] != $route->method || $mismatchKeyFound)
+                            continue;
                         
-                        if(isset($route->action['Layout']))
-                            $layout = $route->action['Layout'];
+                        if(isset($route->action['Controller']))
+                            $this->loadControllerAction($route->action['Controller'], $parameters);
+                        else if(isset($route->action['View']))
+                            $this->loadView($route);
                         
-                        $view = new View;
-                        $view->load($route->action['View'], $layout);
+                        $routeMatch = TRUE;
+                        break;
                     }
-                    
-                    break;
+                    else
+                    {
+                        if($_SERVER['REQUEST_METHOD'] != $route->method)
+                            continue;
+                        
+                        if(isset($route->action['Controller']))
+                            $this->loadControllerAction($route->action['Controller'], $parameters);
+                        else if(isset($route->action['View']))
+                            $this->loadView($route);
+                        
+                        $routeMatch = TRUE;
+                        break;
+                    }
                 }
+            }
+            
+            if(!$routeMatch){
+                header("HTTP/1.0 404 Not Found");
+                die();
             }
         }
         catch(Exception $ex)
         {
             
         }
+    }
+    
+    function loadControllerAction($routeActionValue, $parameters){
+        $controllerAndAction = explode('@', $routeActionValue);
+        $file = 'app/controller/' . $controllerAndAction[0] . '.php';
+
+        if (file_exists($file))
+            require_once $file;
+
+        $controller = new $controllerAndAction[0];
+
+        if(isset($controllerAndAction[1]))
+            call_user_func_array(array($controller, $controllerAndAction[1]), $parameters);
+        else
+            call_user_func_array(array($controller, 'index'), $parameters);
+    }
+    
+    function loadView($route){
+        $layout = 'default';
+
+        if(isset($route->action['Layout']))
+            $layout = $route->action['Layout'];
+
+        $view = new View;
+        $view->load($route->action['View'], $layout);
     }
 }
 
--- Revense/route.php
+++ Revense/route.php
@@ -1,9 +1,9 @@
 <?php
 
 $routeCollection = new RouteCollection(array(
-    new Route('test1', '/admin/category/add/{test2}/{test3}', array('Controller' => 'main')),
-    new Route('test1', '/admin/category/add/', array('Controller' => 'main@index2')),
-    new Route('test2', '/admin/category/edit', array('Controller' => 'main@index2')),
+    new Route('test1', '/admin/category/add/{test2}/{test3}', array('Controller' => 'main'), 'PUT'),
+    new Route('test1', '/admin/category/add/', array('Controller' => 'main@index2')), // default GET
+    new Route('test2', '/admin/category/edit', array('Controller' => 'main@index2'), 'POST'),
     new Route('test3', '/admin/category/update', array('View' => 'index/index2', 'Layout' => 'admin'))
 ));
 
